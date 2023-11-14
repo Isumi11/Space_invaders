24d1dcfa207b3116ab47a77bc823720e07b3097b
Isumi11 <12PeirisI@student.smchull.org>
2023-11-14 11:34:46 +0000

    14/11/2023

--- Space_invaders/Player.cs
+++ Space_invaders/Player.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Drawing;
 
 namespace Space_invaders
 {
     public class Player : Sprite
     {
         private int lives = 3;
+        private KeyboardState myKeyBoard;
+        private bool gamePlaying;
 
         public Player() : base()
         {
@@ -23,8 +26,34 @@ namespace Space_invaders
         {
             
         }
+        protected override void Initialize()
+        {
+            base.Initialize();
+            position = new Vector2(graphics.GraphicsDevice.Viewport.
+            Width / 2 - 64,
+                                    graphics.GraphicsDevice.Viewport.
+                                    Height / 2 - 64);
+        }
         public override void update(GameTime gameTime)
         {
+            //do movement using the keyboard
+            KeyboardState state = Keyboard.GetState();
+
+            if (state.IsKeyDown(Keys.Escape))
+                Exit();
+            if (state.IsKeyDown(Keys.Right))
+                spritePosition.X += 10;
+            if (state.IsKeyDown(Keys.Left))
+                spritePosition.X -= 10;
+            if (state.IsKeyDown(Keys.Up))
+                PlayerP.Y -= 10;
+            if (state.IsKeyDown(Keys.Down))
+                spritePosition.Y += 10; 
+            
+            base.update(gameTime);
+
+
+
 
         }
 
