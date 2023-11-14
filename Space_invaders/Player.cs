f944f14d390fbb548426df7221a0fc55275f36a4
Isumi11 <12PeirisI@student.smchull.org>
2023-11-14 10:47:16 +0000

    Fixed syntax errors

---
+++ Space_invaders/Player.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Space_invaders
+{
+    public class Player : Sprite
+    {
+        private int lives = 3;
+
+        public Player() : base()
+        {
+        }
+        public Player(Texture2D spritetexture, Vector2 spritePosition, Rectangle spriteBox, Color spriteColour)
+            : base(spritetexture, spritePosition, spriteBox, spriteColour)
+        {
+             _spriteBox = spriteBox;
+            _spriteColour = spriteColour;
+            _spritePosition = spritePosition;
+            _spritetexture = spritetexture;
+        }
+        public void FireBullet()
+        {
+            
+        }
+        public override void update(GameTime gameTime)
+        {
+
+        }
+
+        public int Lives
+        {
+            get { return lives; }
+            set { lives = value; }
+            
+        }
+
+    }
+}
---
+++ Space_invaders/Sprite.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Space_invaders
+{
+    public class Sprite
+    {
+        protected Texture2D _spritetexture;
+        protected Vector2 _spritePosition;
+        protected Rectangle _spriteBox;
+        protected Color _spriteColour;
+
+        public Sprite()
+        { }
+
+        public Sprite(Texture2D spritetexture, Vector2 spritePosition, Rectangle spriteBox, Color spriteColour)
+        {
+            _spritetexture = spritetexture;
+            _spritePosition = spritePosition;
+            _spriteBox = BoundingBox;
+            _spriteColour = spriteColour;
+        }
+        public virtual void update(GameTime gameTime)
+        {
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_spritetexture, _spritePosition, _spriteColour);
+        }
+        public Vector2 Position
+        {
+            get { return _spritePosition; }
+            set { _spritePosition = value; }
+        }
+        public Rectangle BoundingBox
+        {
+            get { return _spriteBox; }
+            set { _spriteBox = value; }
+
+        }
+    }
+}
