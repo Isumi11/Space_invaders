61ec6d0137fd31bc4e8e4a841cf935843c4d43c8
Isumi11 <12PeirisI@student.smchull.org>
2023-11-27 15:36:15 +0000

    enemy

---
+++ Space_invaders/Content/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using SharpDX.Direct3D11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_invaders.Content
+{
+    internal class Enemy
+    {
+        private Texture2D
+    }
+}
