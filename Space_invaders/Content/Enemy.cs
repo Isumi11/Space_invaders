4154608283e93fe24393dcdffe3cd8a08d1caf2f
Isumi11 <12PeirisI@student.smchull.org>
2023-12-14 10:19:49 +0000

    enemy

--- Space_invaders/Content/Enemy.cs
+++ Space_invaders/Content/Enemy.cs
@@ -2,6 +2,8 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Space_invaders.Content;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
 namespace Space_invaders.Content
 {
     internal class Enemy : Sprite
@@ -10,15 +12,51 @@ namespace Space_invaders.Content
         protected Vector2 _enemyposition;
         protected Rectangle _enemyBox;
         protected Color _enemyColour;
+        
+       
+
+        public bool IsDrawn { get; internal set; }
+
         public Enemy()
         { }
 
-        public Enemy(Vector2 enemyposition, Rectangle boundingBox, Color enemyColour)
+        public Enemy(Texture2D enemyTexture, Vector2 enemyposition, Rectangle boundingBox, Color enemyColour)
         {
             _enemyposition = enemyposition;
             _enemyBox = boundingBox;
             _enemyColour = enemyColour;
+            _enemyTexture = enemyTexture;
+             
         }
-       
+
+        public Enemy(Vector2 spritePosition, Rectangle boundingBox, Color spriteColour) : base(spritePosition, boundingBox, spriteColour)
+        {
+        }
+
+        public class enemyColour
+        {
+            public static readonly enemyColour Black = new(0, 0, 0);
+            public static readonly enemyColour White = new(255, 255, 255);
+            public static readonly enemyColour Red = new(255, 0, 0);
+            public static readonly enemyColour Green = new(0, 255, 0);
+            public static readonly enemyColour Blue = new(6, 5, 255);
+
+            public byte R;
+            public byte G;
+            public byte B;
+
+            public enemyColour(byte r, byte g, byte b)
+            {
+                R = r;
+                G = g;
+                B = b;
+            }
+        }
+        public void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(_enemyTexture, _enemyposition, _enemyColour);
+        }
+         
+
     }
 }
--- Space_invaders/Game1.cs
+++ Space_invaders/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Space_invaders.Content;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
 
 namespace Space_invaders
 {
@@ -12,8 +13,11 @@ namespace Space_invaders
         private SpriteFont myFont;
 
         private Player firstPlayer;
+        Enemy[,] enemyArray;
         private Bullet firstBullet;
-
+        private Texture2D enemyTexture;
+        private Enemy firstEnemy;
+        bool Isdrawn;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -30,11 +34,16 @@ namespace Space_invaders
             firstPlayer = new Player(new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight - 90),
                 new Rectangle(), Color.White);
             firstBullet = new Bullet(firstPlayer.Position, new Rectangle(), Color.Red, true, firstPlayer);
+            firstEnemy = new Enemy(new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight - 70),
+                new Rectangle(), Color.Blue);
+            DrawAllEnemys(enemyTexture, _graphics.PreferredBackBufferWidth);
+
+
 
 
             base.Initialize();
         }
-
+       
 
         protected override void LoadContent()
         {
@@ -43,11 +52,28 @@ namespace Space_invaders
             firstPlayer.LoadContent(Content, "Placeholder");
             firstBullet.LoadContent(Content, "Bulletplaceholder");
             myFont = Content.Load<SpriteFont>("File");
+            firstEnemy.LoadContent(Content, "enemy911");
+            
+           // DrawAllEnemys(enemyTexture, _graphics.PreferredBackBufferWidth);
 
             // TODO: use this.Content to load your game content here
-           //firstBullet.PositionBullet();
+            //firstBullet.PositionBullet();
         }
+        public void DrawAllEnemys(Texture2D enemyTexture, int screenWidth)
+        {
+            enemyArray = new Enemy[screenWidth / enemyTexture.Width, 5];
 
+            for (int i = 0; i < enemyArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < enemyArray.GetLength(1); j++)
+                {
+                    enemyArray[i, j] = new Enemy(enemyTexture, new Vector2(i * enemyTexture.Width, j * enemyTexture.Height),
+                        new Rectangle(i * enemyTexture.Width, j * enemyTexture.Height, enemyTexture.Width, enemyTexture.Height), Color.White);
+                }
+
+            }
+
+        }
 
         protected override void Update(GameTime gameTime)
         {
@@ -63,7 +89,14 @@ namespace Space_invaders
             firstPlayer.Draw(_spriteBatch);
             firstBullet.Draw(_spriteBatch);
             _spriteBatch.DrawString(myFont, $"X: {firstBullet.Position.X}, Y: {firstBullet.Position.Y}", new Vector2(10, 10), Color.White);
-            
+            foreach (Enemy enemy in enemyArray)
+            {
+                if (enemy.IsDrawn)
+                {
+                    enemy.Draw(_spriteBatch);
+                }
+            }
+
             _spriteBatch.End();
 
             // TODO: Add your drawing code here
